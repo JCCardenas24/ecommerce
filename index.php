032898f321132c214d0577993ac45a3761a6dba3
Esmeralda Nava <ifglitterthen@gmail.com>
2021-05-07 12:52:19 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include 'template/head.php';?>
+  <title>Ansuz | Agente de seguros</title>
+</head>
+
+<body>
+  <div class="container-fluid p-0">
+    <?php include 'template/navbar.php';?>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div>
+            <img src="./assets/images/home-slider/Home-auto.png" class="crop_img d-block w-100" alt="...">
+            <div class="ttl_slide">
+             <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
+             <p  class="text-center">*El precio puede variar al momento de cotizar</p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="./assets/images/home-slider/Home-auto.png" class="crop_img d-block w-100" alt="...">
+          <div class="ttl_slide">
+             <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
+             <p  class="text-center">*El precio puede variar al momento de cotizar</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+          <img src="./assets/images/home-slider/Home-auto.png" class="crop_img  d-block w-100" alt="...">
+          <div class="ttl_slide">
+             <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
+             <p  class="text-center">*El precio puede variar al momento de cotizar</p>
+            </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <div class="blue">
+        
+    </div>
+
+    <div class="incluye">
+<p>Qué Incluye Nuestro Plan</p>
+    </div>
+
+    <div class="preguntas">
+
+    </div>
+
+    <div class="suscribe">
+
+    </div>
+
+    <footer>
+      <div class="redes">
+
+      </div>
+      <div class="legal">
+
+      </div>
+    </footer>
+
+
+
+    <?php include 'template/scripts.php';?>
+  </div>
+</body>
+
+</html>
---
+++ template/navbar.php
@@ -0,0 +1,29 @@
+<nav class="main-nav">
+    <div class="d-flex align-items-center justify-content-between main-nav__primaryRow">
+        <a href="/"><img src="assets/images/logo_ansuz.png" alt="" class="main-nav__primaryRow--logo"></a>
+        <div class="main-nav__primaryRow__tels d-flex align-items-center">
+            <i class="icon-whatsapp"></i>
+            <span class="main-nav__primaryRow__tels--separator">|</span>
+            <span class="main-nav__primaryRow__tels--number"><i class="icon-telefono main-nav__primaryRow__tels--telefono"></i>01 800 6565 3456</span>
+        </div>
+    </div>
+    <div class="d-flex align-items-start justify-content-between main-nav__secondaryRow">
+        <ul class="d-flex main-nav__secondaryRow--menuOptions">
+            <li><a class="active" href="">Inicio</a></li>
+            <li><a href=""> Cotiza ahora</a></li>
+            <li><a href="">Seguros</a></li>
+            <li><a href="">¿Quiénes somos?</a></li>
+            <li><a href="">Promociones</a></li>
+            <li><a href="">FAQ's</a></li>
+            <li><a href="">Blog</a></li>
+        </ul>
+        <div class="d-flex main-nav__secondaryRow__menuActions">
+            <a class="main-nav__secondaryRow__menuActions btnLogin" href="">Iniciar sesión</a>
+            <a class="main-nav__secondaryRow__menuActions btnCotizar" href="">Cotizar ahora</a>
+            <div>
+                <i class="icon-carrito"></i>
+                <span class="badge">3</span>
+            </div>
+        </div>
+    </div>
+</nav>
