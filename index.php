c747cad847cfa32d4c333dac0680e58b11ff0503
Esmeralda <ifglitterthen@gmail.com>
2021-05-10 13:43:52 -0500

    cambios

--- index.php
+++ index.php
@@ -9,40 +9,78 @@
 <body>
   <div class="container-fluid p-0">
     <?php include 'template/navbar.php';?>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <div>
+    <div class="slider_wrapper">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div>
+              <img src="./assets/images/home-slider/Home-auto.png" class="crop_img d-block w-100" alt="...">
+              <div class="ttl_slide">
+                <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
+                <p class="text-center">*El precio puede variar al momento de cotizar</p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
             <img src="./assets/images/home-slider/Home-auto.png" class="crop_img d-block w-100" alt="...">
             <div class="ttl_slide">
               <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
               <p class="text-center">*El precio puede variar al momento de cotizar</p>
             </div>
           </div>
-        </div>
-        <div class="carousel-item">
-          <img src="./assets/images/home-slider/Home-auto.png" class="crop_img d-block w-100" alt="...">
-          <div class="ttl_slide">
-            <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
-            <p class="text-center">*El precio puede variar al momento de cotizar</p>
+          <div class="carousel-item">
+            <img src="./assets/images/home-slider/Home-auto.png" class="crop_img  d-block w-100" alt="...">
+            <div class="ttl_slide">
+              <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
+              <p class="text-center">*El precio puede variar al momento de cotizar</p>
+            </div>
           </div>
         </div>
-        <div class="carousel-item">
-          <img src="./assets/images/home-slider/Home-auto.png" class="crop_img  d-block w-100" alt="...">
-          <div class="ttl_slide">
-            <h1 class="text-center">Hasta 30% <br> <span>de dto y MSI*</span></h1>
-            <p class="text-center">*El precio puede variar al momento de cotizar</p>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="options_seguros_row">
+        <div class="options_row d-flex justify-content-around align-items-center">
+          <div class="icon_text">
+            <i class="icon-autos "></i>
+            <p>Autos</p>
+          </div>
+          <div class="icon_text">
+          <i class="icon-motocicletas"></i>
+            <p>Motocicletas</p>
+          </div>
+          <div class="icon_text">
+          <i class="icon-hogar"></i>
+            <p>Hogar</p>
+          </div>
+          <div class="icon_text">
+          <i class="icon-basicos"></i>
+            <p>Básicos</p>
+          </div>
+          <div class="icon_text">
+          <i class="icon-medicos"></i>
+            <p>Gastos Médicos</p>
+          </div>
+          <div class="icon_text">
+          <i class="icon-accidentes"></i>
+            <p>Accidentes</p>
+          </div>
+          <div class="icon_text">
+          <i class="icon-vida"></i>
+            <p>Vida</p>
+          </div>
+          <div class="icon_text">
+          <i class="icon-pets"></i>
+            <p>Mascotas</p>
           </div>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
 
     <div class="blue">
